f81e3aad7c59f41103752d36d20e132aa5cc400d
innovate.invent@gmail.com <nolan@i2labs.ca>
2018-07-16 22:35:06 -0700

    uasyncio: Add type check to ssl parameter

--- uasyncio/uasyncio/__init__.py
+++ uasyncio/uasyncio/__init__.py
@@ -253,7 +253,7 @@ def start_server(client_coro, host, port, backlog=10, ssl=None):
         if DEBUG and __debug__:
             log.debug("start_server: After accept: %s", s2)
         extra = {"peername": client_addr}
-        if ssl is not None:
+        if ssl is not None and isinstance(ssl, dict):
             ssl['server_side'] = ssl.get('server_side', True) # Set to server side if not specified
             s2 = ussl.wrap_socket(s2, **ssl)
         yield client_coro(StreamReader(s2), StreamWriter(s2, extra))
